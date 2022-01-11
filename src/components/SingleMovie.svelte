d0b4fc72af66e7607db71f8bc27c4322a42a5eda
Bartosz-Kowalczyk <65064726+Bartosz-Kowalczyk@users.noreply.github.com>
2022-01-11 11:26:17 +0100

    font changed

--- src/components/SingleMovie.svelte
+++ src/components/SingleMovie.svelte
@@ -1,51 +1,57 @@
 <script>
-	export let movie;
 
-	let currentMovieGeneres = movie.genres.map((genre) => genre.name);
+	// import { fade } from 'svelte/transition';
+	export let movie;
+    console.log({movie});
+    let currentMovieGeneres = movie.genres.map(genre => genre.name);
 
 	function incrementClaps() {
 		movie.vote_count += 1;
 	}
 
-	function calculateRuntime(runtime) {
-		let hours = Math.floor(runtime / 60);
-		let minutes = runtime % 60;
-		return `${hours}h ${minutes}m`;
-	}
+    function calculateRuntime(runtime) {
+        let hours = Math.floor(runtime / 60);
+        let minutes = runtime % 60;
+        return `${hours}h ${minutes}m`;
+    }
+
 </script>
 
 <div class="card lg:card-side card-bordered shadow-2xl col-span-1">
-	<div class="card-body">
-		<h2 class="card-title">
-			{movie.title} ({movie.release_date.substring(0, 4)})
-			{#each currentMovieGeneres as genre}
-				<div class="badge badge-lg badge-accent mx-2">{genre}</div>
-			{/each}
-		</h2>
-		<p>{movie.overview}</p>
-		<div class="card-actions justify-end">
-			<button class="btn btn-accent" on:click={incrementClaps}>
-				❤
-				<div class="badge ml-2 badge-outline">{movie.vote_count}</div>
-			</button>
-		</div>
-		<div class="w-full shadow stats border border-base-300">
-			<div class="stat shadow-xl">
-				<div class="stat-title">Dochód</div>
-				<div class="stat-value">{movie.revenue.toLocaleString('pl-PL')} USD</div>
-			</div>
-			<div class="stat shadow-xl">
-				<div class="stat-figure text-secondary" />
-				<div class="stat-title">Budżet</div>
-				<div class="stat-value">{movie.budget.toLocaleString('pl-PL')} USD</div>
-			</div>
-			<div class="stat shadow-xl">
-				<div class="stat-title">Czas</div>
-				<div class="stat-value">{calculateRuntime(movie.runtime)}</div>
-			</div>
-		</div>
-	</div>
-	<figure>
-		<img src="https://www.themoviedb.org/t/p/w220_and_h330_face/{movie.poster_path}" alt="{movie.title}" />
-	</figure>
-</div>
+
+    <div class="card-body">
+      <h2 class="card-title text-4xl font-bold">{movie.title} ({movie.release_date.substring(0, 4)})
+        {#each currentMovieGeneres as genre }
+            <div class="badge badge-lg badge-accent mx-2">{genre}</div>
+        {/each}
+        </h2>
+      <p class="text-lg">{movie.overview}</p> 
+      <div class="card-actions justify-end">
+		<button class="btn btn-accent" on:click={incrementClaps}>
+			❤
+			<div class="badge ml-2 badge-outline">{movie.vote_count}</div>
+		</button>
+      </div>
+      <div class="w-full shadow stats border border-base-300">
+        <div class="stat shadow-xl">
+          <div class="stat-title">Dochód</div> 
+          <div class="stat-value font-medium">{movie.revenue.toLocaleString("pl-PL")} USD</div> 
+        </div> 
+        <div class="stat shadow-xl">
+          <div class="stat-figure text-secondary">
+          </div> 
+          <div class="stat-title">Budżet</div> 
+          <div class="stat-value font-medium">{movie.budget.toLocaleString("pl-PL")} USD</div> 
+        </div> 
+        <div class="stat shadow-xl">
+          <div class="stat-title">Czas</div> 
+          <div class="stat-value font-medium">{calculateRuntime(movie.runtime)}</div> 
+        </div>
+      </div>
+      
+    </div>
+    <figure>
+        <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/{movie.poster_path}">
+      </figure> 
+  </div>
+
